5e5c02312e8303d90c5a006c83635186fb632fb8
Madhusudan.C.S <madhusudancs@google.com>
2016-07-09 17:27:57 -0700

    Updates to federation directory.

--- federation/genconfig.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-set -o errexit
-set -o nounset
-set -o pipefail
-
-for i in seq 1 $(grep federation.num_clusters .config | sed -e 's/^federation\.num_clusters=//g'); do
-  CONFIG_="cluster$i." .tmp/conf Kconfig
-done
